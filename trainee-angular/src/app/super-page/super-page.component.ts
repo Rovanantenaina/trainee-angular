2e8455aa7e92fe0c6860ee80d779fef45167ca74
Elie RASOANAIVO <el.rasoanaivo@gmail.com>
2019-07-05 11:16:14 +0200

    formation architecture angular (Resolver, clean Code)

--- trainee-angular/src/app/super-page/super-page.component.ts
+++ trainee-angular/src/app/super-page/super-page.component.ts
@@ -1,12 +1,15 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {CounterService} from '../service/counter.service';
+import {BehaviorSubject, interval, Observable, of, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-super-page',
   template: `
     <p>
-      super-page works!
+      super-page works!<br/>
+      <input type="text" (change)="changeText($event)">
+      <label *ngIf="nom">Bonjour {{nom}} !</label>
       <app-lazy-page [countLazy]="count" (updateCount)="reInit($event)"></app-lazy-page>
     </p>
 
@@ -14,8 +17,12 @@ import {CounterService} from '../service/counter.service';
     <button (click)="recap()">historique</button>
   `
 })
-export class SuperPageComponent implements OnInit {
+export class SuperPageComponent implements OnInit, OnDestroy {
   public count: number;
+  private subscription: Subscription;
+  nom: string;
+
+  private changed: BehaviorSubject<any> = new BehaviorSubject(void 0);
 
   constructor(private router: Router, private counterService: CounterService) {
   }
@@ -26,6 +33,19 @@ export class SuperPageComponent implements OnInit {
   }
 
   ngOnInit() {
+    const observable = interval(1000);
+    this.subscription = observable.subscribe((valeur: any) => {
+      // console.log(valeur);
+    });
+
+    this.changed.subscribe(value => {
+      console.error(value);
+      this.nom = value;
+    });
+  }
+
+  unsubsribeTimer() {
+    this.subscription.unsubscribe();
   }
 
   increment() {
@@ -35,5 +55,13 @@ export class SuperPageComponent implements OnInit {
 
   reInit(event: number) {
     this.counterService.reInit(event);
+  }
+
+  ngOnDestroy(): void {
+    this.unsubsribeTimer();
+  }
+
+  changeText(event: any) {
+    this.changed.next(event.target.value);
   }
 }
